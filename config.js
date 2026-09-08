62724873571550d0bdc5d359d58e2b4e61134ffd
manidecorationtlg-ux <manidecorationtlg@gmail.com>
2026-09-08 19:29:48 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-const SUPABASE_URL = "https://mratyuowwaibihukguic.supabase.co";
-
-const SUPABASE_PUBLISHABLE_KEY =
-  "sb_publishable___O0MOd2VBbu_1vFGNvUDw_yMVh7DCr";
+const SUPABASE_URL = "آپ کا URL";
+const SUPABASE_PUBLISHABLE_KEY = "HIDDEN";
