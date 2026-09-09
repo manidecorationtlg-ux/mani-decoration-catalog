b5a3ee84e65cc7061aeb0afed0e2890fcad3f14f
manidecorationtlg-ux <manidecorationtlg@gmail.com>
2026-09-09 09:13:11 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = "https://........supabase.co";
+const SUPABASE_URL = "https://mratyuowwaibihukguic.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "eyJ........";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable___O0MOd2VBbu_1vFGNvUDw_yMVh7DCr";
